c76139cfd6a98a62116c12418357896b456f4962
Jurica Stanko <stanko64@icloud.com>
2017-09-20 09:16:08 +0200

    Add files via upload

--- uvjetne_strukture/uvjetne2.php
+++ uvjetne_strukture/uvjetne2.php
@@ -10,8 +10,26 @@
     $d=10;
     if($d<$c || $d>$c && $c>$b){
         echo"istina";
-
     }
+    switch($c==$b+6){
+        case($c):
+            echo"<br> b je za 6 manji od c";
+            break;
+        case($b):
+            echo"b je jednak c";
+            break;
+    }
+    switch($d==$c+2){
+        case($d):
+            if($d%5==0){
+                if($c%2==0){
+                    echo"<br> c je za 2 manji od d,d je dijeljiv sa 5, a c sa 2";
+                }
+                else if($c%2!=0){
+                    echo"<br>c je za 2 manji od d,d je dijeljiv sa 5, a c nije dijeljiv sa 2";
+                }
+            }
+        }
 
 ?>
 </body>
