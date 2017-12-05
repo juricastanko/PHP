294273f873fa1d987478ac921fb32f0160fc4d8b
Jurica Stanko <stanko64@icloud.com>
2017-12-05 17:22:11 +0100

    Update skripta.php

--- mail/skripta.php
+++ mail/skripta.php
@@ -2,6 +2,7 @@
     $email=POST["email"];
     $naslov=POST["naslov"];
     $tekst=POST["tekst"];
+    $provjera="@"
 
     echo "Primatelj: ".$email."<br>";
     echo "Naslov: ".$naslov."<br>";
@@ -9,6 +10,10 @@
 
     ini_set("SMTP","smtp.gmail.com");
     ini_set("sendmail_from","tevelly@gmail.com");
+    if(preg_match($provjera, $email)){
+        echo"mail je dobar";}
+    else{
+        echo "mail nije dobar"
 
     if(mail($email, $naslov, $tekst)){
         echo "poruka je uspjesno poslana.<br>";
